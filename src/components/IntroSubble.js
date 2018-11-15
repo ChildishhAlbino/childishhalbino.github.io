4dd350f9ba26942be4e4311867e42c60d34fe6f3
Connor <connorwilliams.home@gmail.com>
2018-11-15 21:03:51 +1100

    [Connor] Update intro subble. Fix styling. Font scales better.

--- src/components/IntroSubble.js
+++ src/components/IntroSubble.js
@@ -1,34 +1,46 @@
-import React from 'react'
-import {Link} from 'gatsby'
-import './introSubble.scss'
-import Subble from './subble'
-import headerPhoto from '../images/me.jpg'
-import style from './subble.module.scss'
+import React from "react";
+import { Link } from "gatsby";
+import customStyle from "./introSubble.module.scss";
+import Subble from "./subble";
+import headerPhoto from "../images/me.jpg";
+import style from "./subble.module.scss";
 
 const IntroSubble = () => (
-  <Subble styling = {[style.One, style.shadow,]}>
-    <div>
-      <div className="introHeader">
-        <h1>
-          Hi! I'm <i>Connor Williams.</i>
-        </h1>
-      </div>
-      <div className="introBlurb">
-        <img src = {headerPhoto} alt="me.jpg" />
-        <div className="blurbText">
-          <p>
-            I'm a Software Developer from Sydney who recently graduated with a
-            Diploma of Software Development. This is a personal blog for me to learn
-            web development and showcase my <Link to="/projects/">projects.</Link>
-          </p>
-          <p>
-            <br />
-            Feel free to learn some more about me <Link to="/about/">here!</Link>
-          </p>
+  <Subble styling={[style.One, style.shadow]}>
+    <div className={customStyle.introSubble}>
+      <Subble
+        styling={[
+          style.navBar,
+          style.shadow,
+          customStyle.introHeading
+        ]}
+      >
+        <Subble styling={[style.navBarItem, style.shadow]}>
+          <h1>
+            Hi, I'm <i>Connor Williams!</i>
+          </h1>
+        </Subble>
+      </Subble>
+      <Subble styling={[style.Two, style.shadow]}>
+        <div className={customStyle.blurb}>
+          <img className={style.shadow} src={headerPhoto} alt="me.jpg" />
+          <div className = {[customStyle.blurbText]}>
+            <p>
+              I'm a Software Developer from Sydney who recently graduated with a
+              Diploma of Software Development. This is a personal blog for me to
+              learn web development and showcase my{" "}
+              <Link to="projects">projects.</Link>
+            </p>
+            <br></br>
+            <p>
+              Feel free to learn some more about me{" "}
+              <Link to="about">here!</Link>
+            </p>
+          </div>
         </div>
-      </div>
+      </Subble>
     </div>
   </Subble>
-)
+);
 
-export default IntroSubble
+export default IntroSubble;
