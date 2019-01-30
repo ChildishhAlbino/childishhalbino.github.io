494d8d7b71d13b70edb4d2750995ef2326706afb
Connor <connorwilliams.home@gmail.com>
2019-01-30 19:48:47 +1100

    [Connor] Update Summary.

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,7 +15,7 @@ const AboutPage = () => (
         <Subble styling={[style.Two, style.shadow]}>
           <BioSubble toggle={true} title="Summary:">
             <p>
-              I'm Connor, a Software Developer from Western Sydney. I'm 19-years-old and passionate about programming and Software Developement. I'm an open-minded person, who works well both individually and as part of a team. I'm motivated and eager to learn how to become a great developer. I'm currently not working in the industry but I'm looking to start. I'm proficient in langauges like Java and C# and competent with web-based languages / frameworks like NodeJS, React / GatsbyJS, etc. I'm an adept problem solver and a quick learner.
+              I'm Connor, a Software Developer from Western Sydney. I'm 19-years-old and passionate about programming and Software Developement. I'm an open-minded person, who works well both individually and as part of a team. I'm motivated and eager to learn how to become a great developer. I'm currently not working in the industry but I'm looking to start. I'm proficient in langauges such as Java and C# and competent with web-based languages / frameworks including NodeJS, React / GatsbyJS, etc. I'm an adept problem solver and a quick learner.
             </p>
           </BioSubble>
           <BioSubble toggle={false} title="More than the tl;dr:">
