064be33b08ec73114e6c0d5ea88d736ac25b44f2
Connor <connorwilliams.home@gmail.com>
2019-02-15 15:34:09 +1100

    [Connor] Add contact page.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,30 @@
 import React from 'react';
-
 import Layout from '../components/layout/layout.js';
 import SEO from '../components/seo';
+import { FaGithub, FaLinkedin, FaEnvelope, FaTwitter } from 'react-icons/fa';
 
 const ContactPage = () => (
 	<Layout>
 		<SEO title="Home" keywords={[ `gatsby`, `application`, `react` ]} />
-		<h1>Contact</h1>
+		<h1>Want to contact me? Try here :D</h1>
+		<div className="icons">
+			<div className="icon">
+				<FaGithub />
+				<p>/childishhalbino</p>
+			</div>
+			<div className="icon">
+				<FaEnvelope />
+				<p>connorandrewwilliams.work@gmail.com</p>
+			</div>
+			<div className="icon">
+				<FaLinkedin />
+				<p>/childishhalbino</p>
+			</div>
+			<div className="icon">
+				<FaTwitter />
+				<p>@childishhalbino</p>
+			</div>
+		</div>
 	</Layout>
 );
 
